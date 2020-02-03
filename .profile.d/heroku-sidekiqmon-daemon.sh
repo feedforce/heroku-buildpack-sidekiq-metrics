3bb073cbda1b583b90847ea7ab5d6b8e828fbaef
Takashi MASUDA <masutaka.net@gmail.com>
2020-02-03 18:59:42 +0900

    Fixup

--- .profile.d/heroku-sidekiqmon-daemon.sh
+++ .profile.d/heroku-sidekiqmon-daemon.sh
@@ -2,16 +2,17 @@
 
 SIDEKIQMON=${1:-/app/vendor/bundle/bin/sidekiqmon}
 
+# don't do anything if we don't have a metrics url.
+if [[ -z "$REDIS_URL" ]] || [[ "${DYNO}" = run\.* ]]; then
+  exit 0
+fi
+
 if [ ! -x "$SIDEKIQMON" ]; then
   echo "No sidekiqmon executable found. Not starting."
   exit 1
 fi
 
 sidekiqmond() {
-  # don't do anything if we don't have a metrics url.
-  if [[ -z "$REDIS_URL" ]] || [[ "${DYNO}" = run\.* ]]; then
-    return 0
-  fi
 
   while true; do
     "$SIDEKIQMON"
